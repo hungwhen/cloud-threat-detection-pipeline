cf0d412739bcffedc6d6392ba44e65c9763aa947
hungwhen <hungwhen@gmail.com>
2025-11-03 16:59:27 -0600

    Initialize Terraform with AWS provider
    Added Terraform configuration for AWS provider.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,12 @@
+#background environment stuff
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.4.0"
+}
 
