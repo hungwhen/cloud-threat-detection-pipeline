227367a3e17ea0083899133664e6e7b219026688
hungwhen <hungwhen@gmail.com>
2025-11-03 17:07:10 -0600

    Add AWS provider and S3 bucket resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,30 @@ terraform {
   required_version = ">= 1.4.0"
 }
 
+provider "aws" {
+  region = var.region
+  // profile = var.aws_profile
+}
+
+// resource - resource type - resource name
+
+resource "aws_s3_bucket" "cloudtrail_logs" {
+
+  bucket = "ctd-cloudtrail-logs-${var.account_id}-${var.region}" // name
+  acl = "private" // no public read/write (only me access)
+  force_destroy = false // no delete if theres objects in the bucket...
+
+  // more anti deletion
+  lifecycle = {
+    prevent_destroy = true
+  }
+
+}
+
+resource "aws_cloudtrail" "main" {
+  name =                                     "ctd-trail"
+  s3_bucket_name =                           aws_s3_bucket.cloudtrail_logs.id
+  include_global_services_events =           true
+  is_multi_region_trail =                    true
+  enable_log_file_validation =               true
+}
