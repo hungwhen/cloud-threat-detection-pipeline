4bcf850f2d033b589c3b9f370f2af76b9f4827df
hungwhen <hungwhen@gmail.com>
2025-11-11 15:16:31 -0600

    Add public access block for S3 buckets
    Added public access block configurations for S3 buckets.

--- terraform/main.tf
+++ terraform/main.tf
@@ -275,3 +275,19 @@ resource "aws_glue_catalog_table" "cloudtrail_logs" {
     "classification" = "json"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "cloudtrail_logs" {
+  bucket = aws_s3_bucket.cloudtrail_logs.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}  
+
+resource "aws_s3_bucket_public_access_block" "athena_results" {
+  bucket = aws_s3_bucket.athena_results.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
